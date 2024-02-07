814a014531a0d1388231b3aee554550d1b3356b4
tmgranata <granatathiago@gmail.com>
2024-02-07 08:03:26 -0300

    fix: types in avatar component

--- src/app/components/header/avatar.tsx
+++ src/app/components/header/avatar.tsx
@@ -5,13 +5,14 @@ import Image from 'next/image';
 import { Button } from '../ui/button';
 import { LogOut } from 'lucide-react';
 import { DropdownMenuItem } from '@radix-ui/react-dropdown-menu';
+import { User } from 'next-auth';
 
-const Avatar = ({ user }: { user: any }) => {
+const Avatar = ({ user }: { user: User }) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant={'ghost'}>
-            <Image className='rounded-full' src={user.image} alt={user.name} width={48} height={48} />
+            <Image className='rounded-full' src={user.image || 'https://source.boringavatars.com/'} alt={user.name || 'Unnamed'} width={48} height={48} />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
