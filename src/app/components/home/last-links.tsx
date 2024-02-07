300a7fa05333ee9c5070894171d7baaee1f012ef
tmgranata <granatathiago@gmail.com>
2024-02-07 08:27:27 -0300

    fix: wrap links in main page

--- src/app/components/home/last-links.tsx
+++ src/app/components/home/last-links.tsx
@@ -12,7 +12,7 @@ const LastLinks = async() => {
     <div className='bottom-0 left-1/2 right-1/2 -translate-x-1/2 w-3/4 h-[30rem] absolute bg-gradient-to-bl from-white to-transparent rounded-t-xl overflow-y-hidden shadow-2xl shadow-white/90 p-[2px]'>
       <div className='w-full h-full bg-gradient-to-br from-white via-secondary to-white rounded-t-xl p-8 flex flex-row items-start gap-2 '>
 
-      <LinksList className='flex flex-row gap-2' links={links}/>
+      <LinksList className='flex flex-row gap-2 flex-wrap' links={links}/>
 
       </div>
     </div>
