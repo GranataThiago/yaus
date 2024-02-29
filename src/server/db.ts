270db866bf22dba8f1b4332c5d8697ad87e1aac0
tmgranata <granatathiago@gmail.com>
2024-02-29 09:14:16 -0300

    fix: replaced planetscale for turso

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,14 +1,23 @@
 import { PrismaClient } from "@prisma/client";
-
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 import { env } from "@/env";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const libsql = createClient({
+  url: `${process.env.DATABASE_URL}`,
+  authToken: `${process.env.DATABASE_AUTH_TOKEN}`,
+})
+
+const adapter = new PrismaLibSQL(libsql)
+
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
